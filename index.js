edbf42a957dd3d93c3440973f7589df930c791e2
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2018-07-07 15:26:07 +0530

    Documented

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-var crypto = require('crypto');
-exports.make = function(req,res) {
-    return {
-        basic : function(key, txnid, amount, productinfo, firstname, email, salt) {
-            var shasum = crypto.createHash('sha512'),
-            dataSequence = key + '|' + txnid + '|' + amount + '|' + productinfo + '|' + firstname + '|' + email + '|||||||||||' + salt;
-            var resultKey = shasum.update(dataSequence).digest('hex');
-            var data = JSON.stringify(resultKey);
-            return data;       
-        }
-    };
-}
+/**
+ * @param {Object} req Request Object
+ * @param {Object} res Response Object
+ * @returns
+ */
+exports.make = function(req, res) {
+  return {
+    /**
+     * @param {String} key
+     * @param {String} txnid
+     * @param {Number | String} amount
+     * @param {String} productinfo
+     * @param {String} firstname
+     * @param {String} email
+     * @param {String} salt
+     * @returns sha-key
+     */
+    basic: function(key, txnid, amount, productinfo, firstname, email, salt) {
+      var shasum = crypto.createHash("sha512"),
+        dataSequence =
+          key +
+          "|" +
+          txnid +
+          "|" +
+          amount +
+          "|" +
+          productinfo +
+          "|" +
+          firstname +
+          "|" +
+          email +
+          "|||||||||||" +
+          salt;
+
+      var resultKey = shasum.update(dataSequence).digest("hex");
+      var data = JSON.stringify(resultKey);
+      return data;
+    }
+  };
+};
